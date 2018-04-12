b0b24b0fc7226de730ff2b259650ce2fbb9d6402
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-04-12 12:26:57 -0700

    readme and tree updated

---
+++ anthony-timea/README.md
@@ -0,0 +1,24 @@
+## Lab-03 - jQuery-with-events
+
+# Author
+
+Timea Heidenreich & Anthony Green
+
+# Overview
+
+We have created a blog, with sort options for the articles, and tabs for different infoemation. Pur blog is informative, pleasing to the eye and easy to use.
+
+# Getting Started
+
+We started wih a starter code provided, went through the TODO list and started to solve them in order
+
+# Architecture
+
+The site was built using the best practice of HTML, CSS, and the jQuery library of javascript.
+
+# Change Log
+
+12:00 Swapped driver-navigator rolls, after functionality of filters has been established
+
+# Credits and Collaborations
+
--- anthony-timea/scripts/articleView.js
+++ anthony-timea/scripts/articleView.js
@@ -40,7 +40,7 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // DONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       $('article').hide();
       // !$(`article[data-author="${$(this).val()}"]`).hide();
       $(`article[data-author="${$(this).val()}"]`).fadeIn(200);
@@ -48,11 +48,11 @@ articleView.handleAuthorFilter = function() {
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
-      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').show();
-
     }
-    $('#category-filter').val('');
+
+    $('#author-filter').val('');
   });
 };
 
@@ -76,6 +76,12 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function() {
   // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  $('.main-nav .tab').click(function(event){
+    event.preventDefault();
+
+
+
+  })
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
@@ -95,4 +101,6 @@ articleView.setTeasers = function() {
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 });
